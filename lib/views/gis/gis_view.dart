236d77d151a9e4758646719d411169d59c70f506
Rachmat <uludsip@gmail.com>
2021-08-09 19:32:47 +0700

    Add map in gis

---
+++ lib/views/gis/gis_view.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GisView extends StatefulWidget {
+  @override
+  _GisViewState createState() => _GisViewState();
+}
+
+class _GisViewState extends State<GisView> {
+  GoogleMapController controller;
+  static const LatLng _center = const LatLng(0.000000, 109.333336);
+  LatLng lastMapPosition = _center;
+  MapType _currentMapType = MapType.normal;
+
+  void _onMapCreate(GoogleMapController controller) {
+    this.controller = controller;
+  }
+
+  _onCameraMove(CameraPosition position) {
+    lastMapPosition = position.target;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: Text(
+            "",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreate,
+          initialCameraPosition: CameraPosition(target: _center, zoom: 20.0),
+          mapType: _currentMapType,
+          onCameraMove: _onCameraMove,
+          zoomControlsEnabled: false,
+        ),
+      ),
+    );
+  }
+}
